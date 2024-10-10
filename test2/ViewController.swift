42d19953a180fa957d3bb9d99210f76dcd0efc4c
Bokhulenkov <bokhulenkov@gmail.com>
2024-10-10 12:04:40 +0300

    initial commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Alexander Bokhulenkov on 10.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
